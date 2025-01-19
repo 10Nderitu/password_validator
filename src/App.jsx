413aa2e78a797f634f73a0b1e9265aca06214fa5
Nderitu10 <raynderitu10@gmail.com>
2025-01-19 14:26:58 +0300

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,47 @@
 import React, { useState } from "react";
-import validator from 'validator';
+import validator from "validator";
 
 const App = () => {
-    const [errorMessage, setErrorMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState("");
 
-    const validate = (value) => {
-        if (validator.isStrongPassword(value, {
-            minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1
-        })) {
-            setErrorMessage('This is a strong password');
-        } else {
-            setErrorMessage('This is not a strong password');
-        }
+  const validate = (value) => {
+    if (
+      validator.isStrongPassword(value, {
+        minLength: 8,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 1,
+      })
+    ) {
+      setErrorMessage("This is a strong password");
+    } else {
+      setErrorMessage("This is not a strong password");
     }
+  };
 
-    return (
-        <div className="container">
-            <h2>Password Strength Checker</h2>
-            <div>
-                <label>Enter Password:</label>
-                <input 
-                    type="password" 
-                    onChange={(e) => validate(e.target.value)}
-                    placeholder="Type your password"
-                />
-                {errorMessage && (
-                    <div className={`message ${errorMessage.includes('strong') ? 'success' : 'error'}`}>
-                        {errorMessage}
-                    </div>
-                )}
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="container">
+      <h2>Check Your Password Strength</h2>
+      <div>
+        <label>Enter Password:</label>
+        <input
+          type="password"
+          onChange={(e) => validate(e.target.value)}
+          placeholder="Enter your password"
+        />
+        {errorMessage && (
+          <div
+            className={`message ${
+              errorMessage.includes("strong") ? "success" : "error"
+            }`}
+          >
+            {errorMessage}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default App;
